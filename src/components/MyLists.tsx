0e492775cadf641c14c5e21e7d35c15c2dc7bbe5
Omar Kasera <omarkasera50@gmail.com>
2024-11-07 15:39:42 +0300

    .

---
+++ src/components/MyLists.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyLists() {
+  return (
+    <div>
+      <div className="flex-1 flex-col bg-white text-gray-800">Omar!!👌👌</div>
+    </div>
+  )
+}
+
+export default MyLists
