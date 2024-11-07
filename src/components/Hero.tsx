bd8b31c12f4c18d1ba53ef4dcab77c2039106e65
Omar Kasera <omarkasera50@gmail.com>
2024-11-07 15:37:01 +0300

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,10 +21,10 @@ function Hero() {
     <div className="px-4 md:px-12 lg:px-20 flex flex-col md:flex-row gap-2 md:gap-3 lg:gap-4">
       <div className="flex-1 flex-col bg-white text-gray-800">Omar!!👌👌</div>
 
-      <div className="bg-blue-400 md:w-fit">
+      <div className="bg-blue-400 md:w-fit p-3 md:p-4 lg:p-6 max-h-[60vh]:">
         <p><span className="mx-4 font-bold">+</span> Add Another List</p>
       </div>
-    </div>
+    </div>  
   );
 }
 
