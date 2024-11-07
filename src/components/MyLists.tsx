7d0f417e959425d583f62de50dee1c2191577279
Omar Kasera <omarkasera50@gmail.com>
2024-11-07 16:19:06 +0300

    .

--- src/components/MyLists.tsx
+++ src/components/MyLists.tsx
@@ -6,11 +6,14 @@ function MyLists(content: Lists) {
 
 
   return (
-    <div>
       <div className="flex-1 flex-wrap justify-start text-gray-800">
-        <div className="bg-white text-black">{first.Title}</div>
+        <div className="bg-white text-black flex-1 max-w-[300px]">
+            <div className="flex flex-row justify-between">
+                <p>{first.Title}</p>
+                <div></div>
+            </div>
+        </div>
       </div>
-    </div>
   );
 }
 
